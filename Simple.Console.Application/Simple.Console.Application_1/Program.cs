18acd48b0d2b91631a5644fe8d001b669626afa9
Yoganjula Reddy Gangarapu <yoganjula.g@hotmail.com>
2021-11-22 13:41:41 +0530

    Added Feature to master

--- Simple.Console.Application/Simple.Console.Application_1/Program.cs
+++ Simple.Console.Application/Simple.Console.Application_1/Program.cs
@@ -11,6 +11,7 @@ namespace Simple.Console.Application_1
         static void Main(string[] args)
         {
             //Hello there, this is my first comment
+            //Feature 2 added
         }
     }
 }
